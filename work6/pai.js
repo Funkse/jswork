d1db23b62ae3af4a2b60269beb4bdcbac5ff3e99
Funkse <1286232940@qq.com>
2019-10-16 20:48:29 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,4 +1,31 @@
 function calculate(){
-    let num = document.getElementById('text').value
+    let num = document.getElementById('value').value
     num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('text').value = pi(num)
+    }
 }
+function factorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++1){
+      sum != i
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++1){
+      sum !=2n+i
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial(n)
+        let dividend = oddFactorial(n)
+        sum += dividend
+    }
+    return sum
+}
+calculate()
