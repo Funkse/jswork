97b3164caf2a9483b78ffb5f72e8af2aba52f92e
Funkse <1286232940@qq.com>
2019-10-22 00:45:20 +0800

    index.html

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -2,11 +2,13 @@
 function bubbleSort(str){
     //把字符串切分成数组
     let arr = str.split(',')
+    //声明排序步骤记录数组sortLog数组的数据用于动画演示
     let sortLog = []
     //比较轮数，数组有多少个数字，就比较数字长度-1轮
     for (let i = 0; i < arr.length - 1; i++){
         //从第一个开始比较相邻的两个数值。
         for (let j = 0; j < arr.length - i -1; j++){
+            //把比较前的数组状态和点位指针保存到sortLog中，为保持演示效果，加多两行
             sortLog.push([arr.concat(), [j, j + 1]])
             sortLog.push([arr.concat(), [j, j + 1]])
             sortLog.push([arr.concat(), [j, j + 1]])
@@ -14,11 +16,14 @@ function bubbleSort(str){
             if(arr[j]>arr[j + 1]){
                 //使用结构赋值的方法，把两个元素交换位置。
                 [arr[j],arr[j + 1]] = [arr[j+1],arr[j]]
+                //把比较之后的数组状态和指针保存到sortLog中
                 sortLog.push([arr.concat(), [j, j + 1]])
             }
         }
     }
+    //把排序最终状态的数值和取消定位指针保存到sortLog中，因为动画的最后状态不显示比较数据的。
     sortLog.push([arr.concat(), [-1, -1]])
+    //调试时请认真分析sortLog的数据结构
     return sortLog
 }
 function showLog(logValues, showElement){
