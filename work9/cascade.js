d989762b3fa66f43d4272ae011fe00f79666cc3e
Funkse <1286232940@qq.com>
2019-10-28 20:06:51 +0800

    work9

--- work9/cascade.js
+++ work9/cascade.js
@@ -1,5 +1,5 @@
-var ollegeSelectc= ['电子与通信学院', '先进制造学院', '电气技术学院', '汽车学院', '计算机与设计学院', '外语商务学院', '经济贸易学院', '物流学院', '马克思主义学院']
-var majorSelect = [
+var collegeSelectFun= ['电子与通信学院', '先进制造学院', '电气技术学院', '汽车学院', '计算机与设计学院', '外语商务学院', '经济贸易学院', '物流学院', '马克思主义学院']
+var majorSelectFun = [
     ['电子', '电信', '通信', '嵌入', '物联'],
     ['数控', '模具', '机设', '机自'],
     ['机电', '电气', '建电', '建智', '空调', '光电', '机器人'],
@@ -10,7 +10,7 @@ var majorSelect = [
     ['物流', '物技', '工企', '营销', '报关', '连锁'],
     ['社工']
 ]
-var classSelect = [
+var classSelectFun = [
     [
         ["电子1801", "电子1802"],
         ["电信1803", "电信1804"],
@@ -75,6 +75,24 @@ var classSelect = [
 ]
 function createOption(obj,data){
     for(var i in data){
-        var 
+        var op = new Option(data[i],i);
+        obj.options.add(op);
     }
+}
+var collegeSelect = document.getElementById('collegeSelect');
+createOption(collegeSelect,collegeSelectFun);
+var majorSelect = document.getElementById('majorSelect');
+collegeSelect.onchange = function(){
+    majorSelect.options.length = 0;
+    createOption(majorSelect,majorSelectFun[collegeSelect.value]);
+    if(collegeSelect.value>=0){
+        majorSelect.onchange();
+    }else{
+        classSelect.options.length=0;
+    }
+}
+var classSelect = document.getElementById('classSelect');
+majorSelect.onchange = function(){
+    classSelect.options.length=0;
+    createOption(classSelect,classSelectFun[collegeSelect.value][majorSelect.value]);
 }
