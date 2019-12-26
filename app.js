c339e83e5d1692d7170dda7382b21704fbd1e20b
Funkse <1286232940@qq.com>
2019-12-26 22:04:29 +0800

    work

--- app.js
+++ app.js
@@ -11,32 +11,29 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('../uploads'))
 const multer = require('multer');
 var storage = multer.diskStorage({
-    //设置上传后文件路径，会自动创建一个upload目录， 与jswork同级目录。
-    destination: function(req, file, cb){
+    destination: function (req, file, cb) {
         cb(null, '../uploads')
     },
-    //给上传文件重命名，获取添加后缀名
-    filename: function (req, file ,cb) {
+    filename: function (req, file, cb) {
         var fileFormat = (file.originalname).split(".");
         cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
     }
 });
-let upload = multer({storage: storage})
-//单文件上传获取信息
-app.post('/upload',upload.single('file'),function(req,res,next){
-    var file = req.file;
-    console.log("original file name is " +file.originalname);
-    console.log("file name is " + file.filename);
-    res.json('/'+file.filename);//这行代码必须要有，否则Browser会处于wait状态。
-})
+let upload = multer({ storage: storage })
 
+app.post('/upload', upload.single('file'), function (req, res, next) {
+    var file = req.file;
+    console.log("original file name is" + file.originalname);
+    console.log("file name is" + file.filename);
+    res.json('/' + file.filename);
+})
 
 app.post('/formBuilder', function (req, res) {
     console.log(req.body)
     res.send(req.body)
 })
 
-let ajaxData = []
+let ajaxData = [];
 let count = 0
 app.post('/ajax', function (req, res) {
     let sno = req.body.sno
@@ -54,25 +51,28 @@ app.post('/ajax', function (req, res) {
     count = ajaxData.length
     res.json(ajaxData)
 })
-app.get('/ajax', function(req, res){
-    let page = req.query.page?Math.max(req.query.page,1):1
+
+app.get('/ajax', function (req, res) {
+    let page = req.query.page ? Math.max(req.query.page, 1) : 1
     let size = 5
-    let maxpage = Math.ceil(ajaxData.length/size)
-    result={data:ajaxData.slice((page-1)*size,page*size),
-     maxPage:maxpage
+    let maxpage = Math.ceil(ajaxData.length / size)
+    result = {
+        data: ajaxData.slice((page - 1) * size, page * size),
+        maxpage: maxpage
     }
     res.json(result)
 })
-app.listen(8080, () => console.log('node express 服务器已启动，监听端口:8080'))
+
+app.listen(8080, () => console.log('node express 服务器已启动,监听端口：8080'))
 
 const openDefaultBrowser = function (url) {
     var exec = require('child_process').exec;
-    switch (process.platform){
+    switch (process.platform) {
         case "darwin":
-            exec('open' + url);
+            exec('open ' + url);
             break;
         case "win32":
-            exec('start' + url);
+            exec('start ' + url);
             break;
         default:
             exec('xdg-open', [url]);
@@ -83,28 +83,28 @@ openDefaultBrowser('http://localhost:8080')
 var ws = require("nodejs-websocket")
 let id = 0
 
-var server = ws.createServer(function (conn){
+var server = ws.createServer(function (conn) {
     id++
     conn.name = "p" + id
-    broadcast(server,'有新人加入。')
-    conn.on("text", function(str){
-        if(str.slice(0,9)=='nickname|'){
-            conn.name=str.split('|')[1]
-            broadcast(server,conn.name+'上线了。')
+    broaadcast(server, '有新人加入.')
+    conn.on("text", function (str) {
+        if (str.slice(0, 9) == 'nickname|') {
+            conn.name = str.split('|')[1]
+            broaadcast(server, conn.name + '上线啦。')
             return
         }
-        broadcast(server,conn.name+':'+str)
+        broaadcast(server, conn.name + ':' + str)
     })
-    conn.on('connect',function(){
+    conn.on('connect', function () {
         conn.name = "name"
     })
-    conn.on("close", function (code, reason){
+    conn.on("close", function (code, reason) {
         console.log("Connection closed")
     })
-}).listen(8081,()=>console.log('socket server listening on:8081'))
+}).listen(8081, () => console.log('socket server listening on:8081'))
 
-function broadcast(server, msg){
-    server.connections.forEach(function (conn){
+function broaadcast(server, msg) {
+    server.connections.forEach(function (conn) {
         conn.sendText(msg)
     })
 }
